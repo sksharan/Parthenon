fd7c20ebe01191e308857fecc534607a732185e7
Shaneel Sharan <shaneel.sharan@gmail.com>
2016-05-03 22:44:20 -0700

    Represent player hunger as food icons, refactoring

--- client/src/main/webapp/js/controller/PlayerController.js
+++ client/src/main/webapp/js/controller/PlayerController.js
@@ -3,17 +3,30 @@ define(['js/ParthenonApp', 'js/service/PlayerService'], function(app) {
             ['$scope', 'PlayerService', function($scope, PlayerService) {
         $scope.players = PlayerService.getPlayers();
 
-        /** Returns the number of full hearts used to represent the player's health. */
-        $scope.numFullHearts = function(health) {
+        /** Returns the number of full heart icons used to represent the player's health. */
+        $scope.numFullHeartIcons = function(health) {
             return Math.floor(health/2);
         }
-        /** Returns the number of half hearts used to represent the player's health. */
-        $scope.numHalfHearts = function(health) {
+        /** Returns the number of half heart icons used to represent the player's health. */
+        $scope.numHalfHeartIcons = function(health) {
             return health % 2;
         }
-        /** Returns the number of empty hearts used to represent the player's health. */
-        $scope.numEmptyHearts = function(health) {
-            return 10 - (Math.floor(health/2)) - (health % 2);
+        /** Returns the number of empty heart icons used to represent the player's health. */
+        $scope.numEmptyHeartIcons = function(health) {
+            return 10 - $scope.numFullHeartIcons(health) - $scope.numHalfHeartIcons(health);
+        }
+
+        /** Returns the number of full hunger icons used to represent the player's health. */
+        $scope.numFullHungerIcons = function(foodLevel) {
+            return Math.floor(foodLevel/2);
+        }
+        /** Returns the number of half hunger icons used to represent the player's health. */
+        $scope.numHalfHungerIcons = function(foodLevel) {
+            return foodLevel % 2;
+        }
+        /** Returns the number of empty hunger icons used to represent the player's health. */
+        $scope.numEmptyHungerIcons = function(foodLevel) {
+            return 10 - $scope.numFullHungerIcons(foodLevel) - $scope.numHalfHungerIcons(foodLevel);
         }
     }]);
 });
